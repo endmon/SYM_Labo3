cbaf25abfdc03f1af4596e2ddc96dd38b8fd43e5
Joshua Gomes Da Costa <joshua.gomesdacosta@heig-vd.ch>
2019-11-22 14:48:36 +0100

    ajout layout

--- app/src/main/java/ch/heigvd/scan/NFC.java
+++ app/src/main/java/ch/heigvd/scan/NFC.java
@@ -1,4 +1,13 @@
 package ch.heigvd.scan;
 
-public class NFC {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NFC extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_nfc);
+    }
 }
